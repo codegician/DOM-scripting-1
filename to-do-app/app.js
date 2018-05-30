abace471d3852b343170cb2c2fa3d5c0c0c583f0
Jeeermss <35412944+Jeeermss@users.noreply.github.com>
2018-05-29 19:13:31 -0700

    Update app.js

--- to-do-app/app.js
+++ to-do-app/app.js
@@ -1,49 +1,36 @@
+//add to do form 
+const addToDoForm = document.getElementById('addToDoForm'); 
 
-    
-    //add to do form 
-    const addToDoForm = document.getElementById('addToDoForm');
-    
-    //new to do text
-    const newToDoText = document.getElementById('newToDoText');
-    
-    //to do list 
-    const toDoList = document.getElementById('toDoList');
-    addToDoForm.addEventListener('submit', event => {
-        event.preventDefault();
-        
-        console.log("click"); 
-        
-        //get the text 
-        let title = newToDoText.value; 
-        
-        //create a new li
-        let newLi = document.createElement('li');
-        
-        // create a new input 
-        let checkbox = document.createElement('input');
-        
-        //set the input's type to checkbox 
-        checkbox.type = "checkbox"; 
-        
-        // set the title 
-        newLi.textContent = title; 
-        
-        // attach the checkbox to the Li 
-        newLi.appendChild(checkbox); 
-        
-        // attach the li to the ul 
-        toDoList.appendChild(newLi);
-        
-        // empty the input
-        newToDoText.value = ''; 
-  
-        var todoName = document.querySelector('[type="text"]').value;
+//new to do text 
+const newToDoText = document.getElementById('newToDoText'); 
 
-function addedFeedback(todoName) {
-  let liveRegion = document.querySelector('[role="status"]');
-  liveRegion.textContent = `${todoName} added.`;
-}
+//to do list 
+const toDoList = document.getElementById('toDoList'); 
+addToDoForm.addEventListener('submit', event => { 
+event.preventDefault(); 
+console.log("click"); 
 
-// example usage
-addedFeedback(todoName);
-    }); 
+//get the text 
+let title = newToDoText.value; 
+
+//create a new li 
+let newLi = document.createElement('li'); 
+
+// create a new input 
+let checkbox = document.createElement('input'); 
+
+//set the input's type to checkbox 
+checkbox.type = "checkbox"; 
+
+// set the title 
+newLi.textContent = title; 
+
+// attach the checkbox to the Li 
+newLi.appendChild(checkbox); 
+
+// attach the li to the ul 
+toDoList.appendChild(newLi); 
+
+// empty the input 
+newToDoText.value = ''; 
+}); 
